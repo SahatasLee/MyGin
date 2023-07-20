fc3fcbbeb3c1224fa8d202ca1b8a14abd79d7050
sahatas <sahatasnutlee@gmail.com>
2023-07-20 13:35:07 +0700

    add

--- db/db.go
+++ db/db.go
@@ -21,7 +21,7 @@ func (l SqlLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql st
 	fmt.Print(sql + "\n ================================= \n")
 }
 func DatabaseInit() *gorm.DB {
-	dsn := "host=localhost user=postgres password=admin dbname=myDatabase port=5432"
+	dsn := "host=10.111.0.124 user=admin password=psltest dbname=postgresdb port=5432"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: &SqlLogger{},
 		DryRun: false,
@@ -32,6 +32,17 @@ func DatabaseInit() *gorm.DB {
 	}
 
 	db.AutoMigrate(models.User{})
+
+	// if err := db.Use(otelgorm.NewPlugin()); err != nil {
+	// 	panic(err)
+	// }
+
+	// tracer := otel.Tracer("app_or_package_name")
+
+	// ctx, span := tracer.Start(ctx, "root")
+	// defer span.End()
+
+	// otelplay.PrintTraceID(ctx)
 
 	return db
 }
--- main.go
+++ main.go
@@ -1,18 +1,110 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"myGin/db"
 	"myGin/routes"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
+
+var (
+	endpoint = "10.111.0.123:55680"
+)
+
+// Initializes an OTLP exporter, and configures the corresponding trace and
+// metric providers.
+func initProvider() (func(context.Context) error, error) {
+	ctx := context.Background()
+
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			// the service name used to display traces in backends
+			semconv.ServiceName("test-service-golang-gin"),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create resource: %w", err)
+	}
+
+	// If the OpenTelemetry Collector is running on a local cluster (minikube or
+	// microk8s), it should be accessible through the NodePort service at the
+	// `localhost:30080` endpoint. Otherwise, replace `localhost` with the
+	// endpoint of your cluster. If you run the app inside k8s, then you can
+	// probably connect directly to the service through dns.
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, endpoint,
+		// Note the use of insecure transport here. TLS is recommended in production.
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create gRPC connection to collector: %w", err)
+	}
+
+	// Set up a trace exporter
+	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create trace exporter: %w", err)
+	}
+
+	// Register the trace exporter with a TracerProvider, using a batch
+	// span processor to aggregate spans before export.
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	// set global propagator to tracecontext (the default is no-op).
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	// Shutdown will flush any remaining spans and shut down the exporter.
+	return tracerProvider.Shutdown, nil
+}
 
 func main() {
 
+	log.Printf("Waiting for connection...")
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	shutdown, err := initProvider()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := shutdown(ctx); err != nil {
+			log.Fatal("failed to shutdown TracerProvider: %w", err)
+		}
+	}()
+
+	// tracer := otel.Tracer("test-tracer-golang")
 	db := db.DatabaseInit()
 
 	route := gin.Default()
+	route.Use(otelgin.Middleware("test-gin"))
 
 	route.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "Hello World"})
